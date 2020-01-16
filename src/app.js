e5694fc527a45e36dc82f5016f3d5faa4394301a
alekseypirogov <iartifical@gmail.com>
2020-01-16 17:28:18 +0700

    added modal windows

--- src/app.js
+++ src/app.js
@@ -37,6 +37,32 @@ const price_slider = tns({
   }
 });
 
+const modalTriggers = document.querySelectorAll('.popup-trigger')
+const modalCloseTrigger = document.querySelector('.popup-modal__close')
+const bodyBlackout = document.querySelector('.body-blackout')
+
+modalTriggers.forEach(trigger => {
+  trigger.addEventListener('click', () => {
+    const { popupTrigger } = trigger.dataset
+    const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
+    popupModal.classList.add('is--visible')
+    bodyBlackout.classList.add('is-blacked-out')
+    document.body.style.overflow = "hidden"
+
+    document.querySelector('.is-blacked-out').addEventListener('click', () => {
+      popupModal.classList.remove('is--visible')
+      bodyBlackout.classList.remove('is-blacked-out')
+      document.body.style.overflow = "auto"
+    })
+
+    popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+       popupModal.classList.remove('is--visible')
+       bodyBlackout.classList.remove('is-blacked-out')
+       document.body.style.overflow = "auto"
+    })
+  })
+})
+
 (function() {
 	scrollTo();
 })();
